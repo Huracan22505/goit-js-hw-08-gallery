a8637c995be47dfef13a15e86a7140db6982812c
Huracan <huracan22505@protonmail.com>
2020-11-21 18:16:38 +0200

    adds overlay close and ESC solutions

--- js/gallery.js
+++ js/gallery.js
@@ -5,6 +5,7 @@ const modalImage = document.querySelector('.lightbox__image');
 const btnCloseModal = document.querySelector(
   'button[data-action="close-lightbox"]',
 );
+const overlayModal = document.querySelector('.lightbox__overlay');
 
 const galleryContainer = document.querySelector('.js-gallery');
 const cardsMarkup = createGalleryCardsMarkup(images);
@@ -13,6 +14,7 @@ galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
 galleryContainer.addEventListener('click', onGalleryContainerClick);
 btnCloseModal.addEventListener('click', onBtnCloseModalClick);
+overlayModal.addEventListener('click', onOverlayModalClick);
 
 function createGalleryCardsMarkup(images) {
   return images
@@ -36,15 +38,35 @@ function createGalleryCardsMarkup(images) {
 
 function onGalleryContainerClick(evt) {
   evt.preventDefault();
+
+  window.addEventListener('keydown', onEscKeyPress);
+
   if (!evt.target.classList.contains('gallery__image')) {
     return;
   }
 
   modal.classList.add('is-open');
+
   modalImage.src = evt.target.dataset.source;
 }
 
 function onBtnCloseModalClick(evt) {
+  window.removeEventListener('keydown', onEscKeyPress);
+
   modal.classList.remove('is-open');
   modalImage.src = '';
+}
+
+function onOverlayModalClick(evt) {
+  modal.classList.remove('is-open');
+  modalImage.src = '';
+}
+
+function onEscKeyPress(evt) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = evt.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    onBtnCloseModalClick();
+  }
 }
